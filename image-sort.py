3c4e99fe79be85f00f9020efc2677424de24e2dd
Andrew Froemming <andrew@froemming.me>
2020-08-13 00:11:28 -0500

    made recursive

--- image-sort.py
+++ image-sort.py
@@ -1,19 +1,28 @@
 from PIL import Image 
 
-from os import listdir, mkdir
+from os import listdir, mkdir, getcwd
+from os.path import join, isdir
 from shutil import move
 
-files = listdir()
-directories = []
+def i_sort(w_path):
+    print(w_path)
+    files = map(lambda x: join(w_path, x), listdir(w_path))
+    directories = []
 
-for f in files:
-    try:
-        im = Image.open(f)
-    except:
-        continue
-    c_dir = str(im.width) + 'x' + str(im.width)
-    im.close()
-    if not c_dir in directories:
-        mkdir(c_dir)
-        directories.append(c_dir)
-    move(f, c_dir)
+    for f in files:
+        print(f)
+        if isdir(f):
+            i_sort(f)
+        try:
+            im = Image.open(f)
+        except:
+            continue
+        c_dir = join(b_path, str(im.width) + 'x' + str(im.width))
+        im.close()
+        if not isdir(c_dir):
+            mkdir(c_dir)
+            directories.append(c_dir)
+        move(f, c_dir)
+
+b_path = getcwd()
+i_sort(b_path)
