f9aef28f7124029298ad841d8af4bf80ace41f58
Amelia Foremming <7919407+afroemming@users.noreply.github.com>
2020-09-23 22:39:13 -0500

    change imports

--- image-sort.py
+++ image-sort.py
@@ -1,26 +1,24 @@
-from PIL import Image 
+import os, shutil
 
-from os import listdir, mkdir, getcwd
-from os.path import join, isdir
-from shutil import move
+from PIL import Image 
 
 def i_sort(w_path):
     print(w_path)
-    files = map(lambda x: join(w_path, x), listdir(w_path))
+    files = map(lambda x: os.path.join(w_path, x), os.listdir(w_path))
 
     for f in files:
         print(f)
-        if isdir(f):
+        if os.path.isdir(f):
             i_sort(f)
         try:
             im = Image.open(f)
         except:
             continue
-        c_dir = join(b_path, str(im.width) + 'x' + str(im.height))
+        c_dir = os.path.join(b_path, str(im.width) + 'x' + str(im.height))
         im.close()
-        if not isdir(c_dir):
-            mkdir(c_dir)
-        move(f, c_dir)
+        if not os.path.isdir(c_dir):
+            os.mkdir(c_dir)
+        shutil.move(f, c_dir)
 
-b_path = getcwd()
+b_path = os.getcwd()
 i_sort(b_path)
